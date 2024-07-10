e4388167ebea6768a42ee51ba49c58bdf78a7785
Deepak kumar <deepaks9631342845@gmail.com>
2024-07-10 13:18:35 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent=React.createElement("div",{id: "parent"},
+   React.createElement("div",{id:"child"}, 
+    React.createElement("h1",{},"I'am ab h1 tag")));
+
+
+
+
+const heading= React.createElement("h1",{id:"heading"}, "hello world from react!");
+   const root=ReactDOM.createRoot(document.getElementById("root"));
+   root.render(heading);
